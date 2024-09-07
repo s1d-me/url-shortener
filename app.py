ffedc67480b420967b164f3fb2a788af865240d0
Sarthak Sidhant <52284479+Sarthak-Sidhant@users.noreply.github.com>
2024-09-08 00:47:13 +0530

    OH NO I MUSTA LOST A BET

--- app.py
+++ app.py
@@ -782,4 +782,5 @@ def use_recovery_code():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.getenv('PORT'))
+    app.run(debug=True, host='0.0.0.0', port=port)
